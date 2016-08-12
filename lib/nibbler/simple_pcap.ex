9510a8536eb6e85c1b31cc8ece731312f4467cb9
Patrick Robinson <patrick.robinson@envato.com>
2016-08-12 22:14:18 +1000

    Simple filter to decode ethernet headers

---
+++ lib/nibbler/simple_pcap.ex
@@ -0,0 +1,42 @@
+defmodule Nibbler.SimplePcap do
+  use GenServer
+
+  def start_link(opts \\ []),
+  do: GenServer.start_link(__MODULE__, :ok, opts)
+
+  def init(opts),
+  do: :epcap.start(opts)
+
+  def handle_info({packet, dlt, _time, _len, data}, _) do
+    headers = decode(dlt, data) |> header
+  end
+
+  # Internal functions
+
+  def decode(dlt, data),
+  do: :pkt.decapsulate({:pkt.dlt(dlt), data})
+
+  def header(payload),
+  do: header(payload, [])
+
+  def header([], acc),
+  do: Enum.reverse(acc)
+
+  def header([{:ether, shost = shost, dhost = dhost}|rest], acc) do
+    rest
+    |> header(
+      [
+        {:ether,
+          [
+            {:source_macaddr, ether_addr(shost)},
+            {:destination_macaddr, ether_addr(dhost)}
+          ]
+        }|acc
+      ]
+    )
+  end
+
+  def ether_addr(mac),
+  do: Base.encode16(mac) |> Enum.join(":")
+
+end
